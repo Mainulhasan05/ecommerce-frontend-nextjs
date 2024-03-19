eb8eb7f13ede70b807cfaf5a10d14596a8599f81
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-19 10:31:48 +0600

    Refactor category sidebar and add dynamic rendering of categories

--- Components/Homepage/CategorySidebar.js
+++ Components/Homepage/CategorySidebar.js
@@ -2,8 +2,10 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
 const Sidebar = () => {
+    const {homedata}=useSelector(state=>state.home)
     const [isOpen, setIsOpen] = useState(true);
 
     const toggleMenu = () => {
@@ -11,7 +13,52 @@ const Sidebar = () => {
     };
 
     return (
-        <div class="hero__categories"><div class="hero__categories__all"><i class="fa fa-bars"></i><span>All departments</span></div><ul id="category_list" class="hidden active" style={{"maxHeight":"451px"}}><li><a href="#">Fresh Meat</a></li><li><a href="#">Vegetables</a></li><li><a href="#">Fruit &amp; Nut Gifts</a></li><li><a href="#">Fresh Berries</a></li><li><a href="#">Ocean Foods</a></li><li><a href="#">Butter &amp; Eggs</a></li><li><a href="#">Fastfood</a></li><li><a href="#">Fresh Onion</a></li><li><a href="#">Papayaya &amp; Crisps</a></li><li><a href="#">Oatmeal</a></li><li><a href="#">Fresh Bananas</a></li></ul></div>
+        <div class="hero__categories">
+            <div class="hero__categories__all">
+                <i class="fa fa-bars"></i>
+                <span>All departments</span>
+            </div>
+            <ul>
+                {
+                    homedata?.categories?.map((category,index)=>(
+                        <li className='parent_categories' >
+                        <Link href={`/category/${category?.slug}`}>{category?.name}</Link>
+                        <div className='sub_categories' >
+                            <ul>
+                                {
+                                    category?.children?.map((sub_category,index)=>(
+                                        <li><Link href={`/category/${sub_category?.slug}`}>{sub_category?.name}</Link></li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                        </li>
+                    ))
+                }
+                {/* <li className='parent_categories' >
+                <a href="#">Fresh Meat</a>
+                <div className='sub_categories' >
+                    <ul>
+                        <li><Link href="/product/1">Meat</Link></li>
+                        <li><Link href="/product/2">Meat</Link></li>
+                        <li><Link href="/product/3">Meat</Link></li>
+                        <li><Link href="/product/4">Meat</Link></li>
+                        <li><Link href="/product/5">Meat</Link></li>
+                    </ul>
+                </div>
+                </li>
+                <li><a href="#">Vegetables</a></li>
+                <li><a href="#">Fruit & Nut Gifts</a></li>
+                <li><a href="#">Fresh Berries</a></li>
+                <li><a href="#">Ocean Foods</a></li>
+                <li><a href="#">Butter & Eggs</a></li>
+                <li><a href="#">Fastfood</a></li>
+                <li><a href="#">Fresh Onion</a></li>
+                <li><a href="#">Papayaya & Crisps</a></li>
+                <li><a href="#">Oatmeal</a></li>
+                <li><a href="#">Fresh Bananas</a></li> */}
+            </ul>
+        </div>
     );
 };
 
