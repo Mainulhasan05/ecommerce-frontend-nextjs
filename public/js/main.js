15eac70d8ff447b14c388ccf2f4c4e0633d033c7
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-29 14:54:13 +0600

    Remove product details pic slider initialization

--- public/js/main.js
+++ public/js/main.js
@@ -65,15 +65,7 @@
         /*---------------------------------
         Product Details Pic Slider
     ----------------------------------*/
-    $(".product__details__pic__slider").owlCarousel({
-        loop: true,
-        margin: 20,
-        items: 4,
-        dots: true,
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true
-    });
+    
 
     /*-----------------------
 		Price Range Slider
